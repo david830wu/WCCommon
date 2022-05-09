c88da51fdcc137337acb732c6b47ca4c06a3ab46
Wentao Wu <david830wu@gmail.com>
2022-05-09 16:54:29 +0800

    add cmake install

--- cmake/WCCommonConfig.cmake
+++ cmake/WCCommonConfig.cmake
@@ -1,8 +1,10 @@
 # add dependencies
 include(CMakeFindDependencyMacro)
 # add all libs imported by find_package() in project
-find_dependency(HDF5)
-find_dependency(Torch)
+find_dependency(date)
+find_dependency(fmt)
+find_dependency(yaml-cpp)
+find_dependency(spdlog)
 
 # config WCCommon
 include("${CMAKE_CURRENT_LIST_DIR}/WCCommonTargets.cmake")
