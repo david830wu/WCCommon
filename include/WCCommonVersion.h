195f394643f4d481caf3b8a5a35d0937e65da2e6
Wentao Wu <david830wu@gmail.com>
2022-07-14 12:03:41 +0800

    update version

--- include/WCCommonVersion.h
+++ include/WCCommonVersion.h
@@ -5,5 +5,5 @@
 
 #pragma once
 
-#define WCCOMMON_VERSION         "1.1.0"
-#define WCCOMMON_VERSION_MESSAGE "add wcc::get_logger to get logger safely" 
+#define WCCOMMON_VERSION         "1.2.0"
+#define WCCOMMON_VERSION_MESSAGE "add wcc::ProgressBar" 
