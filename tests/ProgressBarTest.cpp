0c71ef5ca3a855e249d970588c8e8840434d52bc
Wentao Wu <david830wu@gmail.com>
2022-07-14 11:53:09 +0800

    add ProgressBar test

---
+++ tests/ProgressBarTest.cpp
@@ -0,0 +1,26 @@
+/* ProgressBarTest.cpp
+* 
+* Author: Wentao Wu
+*/
+
+#include "ProgressBar.h"
+
+#include <chrono>
+#include <thread>
+
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+TEST_CASE("ProgressBarTest", "[WCCommon]") {
+    SECTION("show") {
+        std::size_t data_len = 128;
+        wcc::ProgressBar prog_bar(70, std::cout);
+        for(std::size_t i = 0; i < data_len ; ++i) {
+            // proccessing data
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+
+            prog_bar.update( (i + 1) * 100 / data_len );
+        }
+        prog_bar.finish();
+    }
+}
