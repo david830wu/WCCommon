16f554051fd029e4329a5d70d1bc4b347cba5f60
Wentao Wu <david830wu@gmail.com>
2022-12-09 10:38:28 +0800

    add H5IO lib

---
+++ include/H5IO.h
@@ -0,0 +1,145 @@
+/* H5IO.h
+ * simple and user-friendly utils to read and write Hdf5 format file
+ * 
+ * Author: Wentao Wu
+*/
+
+#pragma once
+
+#include "NaNDefs.h"
+#include "NumericTime.h"
+
+#include <hdf5.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace wcc {
+
+//===============================================================================
+// Type conversions
+//===============================================================================
+template <typename T>
+struct H5TypeMatchFalse { enum { value = false }; };
+template<typename T> inline hid_t to_h5_type_id() { 
+    static_assert(H5TypeMatchFalse<T>::value, "Undefined target HDF5 type"); 
+    return H5T_NATIVE_CHAR; 
+}
+template<> inline hid_t to_h5_type_id<bool              >() { return H5T_NATIVE_HBOOL ; } 
+template<> inline hid_t to_h5_type_id<char              >() { return H5T_NATIVE_CHAR  ; } 
+template<> inline hid_t to_h5_type_id<short             >() { return H5T_NATIVE_SHORT ; } 
+template<> inline hid_t to_h5_type_id<int               >() { return H5T_NATIVE_INT   ; } 
+template<> inline hid_t to_h5_type_id<long              >() { return H5T_NATIVE_LONG  ; } 
+template<> inline hid_t to_h5_type_id<long long         >() { return H5T_NATIVE_LLONG ; } 
+template<> inline hid_t to_h5_type_id<unsigned short    >() { return H5T_NATIVE_USHORT; } 
+template<> inline hid_t to_h5_type_id<unsigned int      >() { return H5T_NATIVE_UINT  ; } 
+template<> inline hid_t to_h5_type_id<unsigned long     >() { return H5T_NATIVE_ULONG ; } 
+template<> inline hid_t to_h5_type_id<unsigned long long>() { return H5T_NATIVE_ULLONG; } 
+template<> inline hid_t to_h5_type_id<float             >() { return H5T_NATIVE_FLOAT ; } 
+template<> inline hid_t to_h5_type_id<double            >() { return H5T_NATIVE_DOUBLE; } 
+template<> inline hid_t to_h5_type_id<wcc::NumericTime  >() { return H5T_NATIVE_UINT  ; } 
+
+//===============================================================================
+// scan for objects in group
+//===============================================================================
+inline herr_t _add_group_name_to_keys(hid_t group, const char *name, const H5L_info_t *info, void *op_data) {
+    std::vector<std::string>* p_keys = (std::vector<std::string>*)op_data;
+    p_keys->emplace_back(name);
+    return 0;
+}
+
+// scan for objects in given group_id without recursion
+// e.g. for an hdf5 file contains
+//   /Stocks/AAPL/Transactions
+//   /Stocks/AAPL/Orders
+//   /Stocks/GOOGL/Transactions
+//   /Stocks/GOOGL/Orders
+// given stock_grp as group id at /Stock, `scan_group_objects(stock_grp)` returns 
+//   std::vector<std::string>{"AAPL", "GOOGL"}
+inline std::vector<std::string> scan_group_object(hid_t group_id) {
+    std::vector<std::string> objs;
+    hsize_t idx = 0;
+    herr_t status = H5Literate(group_id, H5_INDEX_NAME, H5_ITER_NATIVE, &idx, &_add_group_name_to_keys, (void*)(&objs));
+    if(status < 0) {
+        throw std::runtime_error("scan_group_objects,H5Literate");
+    }
+    return objs;
+}
+
+// scan for objects in given path group without recursion
+// same as scan_group_object, but accept path string as an argument
+// e.g. for an hdf5 file contains
+//   /Stocks/AAPL/Transactions
+//   /Stocks/AAPL/Orders
+//   /Stocks/GOOGL/Transactions
+//   /Stocks/GOOGL/Orders
+// given opened file id as fid, `scan_path_objects(fid, "/Stocks")` returns 
+//   std::vector<std::string>{"AAPL", "GOOGL"}
+// given /Stock group id as gid, `scan_path_objects(gid, "AAPL")` returns 
+//   std::vector<std::string>{"Transaction", "Orders"}
+inline std::vector<std::string> scan_path_object(hid_t file_id, std::string const& path) {
+    herr_t status;
+    hid_t group_id = H5Gopen(file_id, path.c_str(), H5P_DEFAULT);
+    if(group_id < 0) {
+        throw std::runtime_error("scan_path_object,OpenGroupFailed,group=\""+path+"\"");
+    }
+    return scan_group_object(group_id);
+}
+
+//===============================================================================
+// Basic Write Operations
+//===============================================================================
+template<typename T>
+void write_array_to_h5(hid_t file_id, const std::string& dataset_name, const T* data, std::size_t len) {
+    constexpr std::size_t k_label_rank = 1;
+    hsize_t dims[k_label_rank];
+    dims[0] = len;
+
+    hid_t dataspace_id = H5Screate_simple(k_label_rank, dims, nullptr);
+    if(dataspace_id < 0) { throw std::runtime_error("WriteArray::H5Screate_simple"); }
+
+    hid_t data_type_id = to_h5_type_id<T>();
+
+    hid_t dataset_id = H5Dcreate(file_id, dataset_name.c_str(), data_type_id, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    if(dataset_id < 0) { throw std::runtime_error("WriteArray::H5Dcreate"); }
+
+    herr_t status = H5Dwrite(dataset_id, data_type_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+    if(status < 0) { throw std::runtime_error("WriteArray::H5Dwrite"); }
+
+    if( H5Sclose(dataspace_id) < 0 ) { throw std::runtime_error("WriteArray::H5Sclose"); }
+    if( H5Dclose(dataset_id) < 0 ) { throw std::runtime_error("WriteArray::H5Dclose"); }
+}
+template<typename Container>
+void write_vector_to_h5(hid_t file_id, const std::string& dataset_name, Container const& data, char zip_method) {
+    using value_type = typename Container::value_type;
+    std::vector<value_type> buffer(data.begin(), data.end());
+    write_array_to_h5<value_type>(file_id, dataset_name, buffer.data(), buffer.size());
+}
+template<typename T>
+void write_vector_to_h5(hid_t file_id, const std::string& dataset_name, std::vector<T> const& data, char zip_method) {
+    using value_type = T;
+    write_array_to_h5<value_type>(file_id, dataset_name, data.data(), data.size());
+}
+
+template<typename T>
+void read_vector_from_h5(hid_t file_id, const std::string& dataset_name,
+                     std::vector<T>& data) {
+    constexpr std::size_t k_label_rank = 1;
+    hsize_t dims[k_label_rank];
+    hid_t data_type_id = to_h5_type_id<T>();
+
+    hid_t dataset_id = H5Dopen(file_id, dataset_name.c_str(), H5P_DEFAULT);
+    if(dataset_id < 0) { throw std::runtime_error("ReadArray::H5Dopen"); }
+    hid_t dataspace_id = H5Dget_space(dataset_id);
+    if(dataspace_id < 0) { throw std::runtime_error("ReadArray::H5Dget_space"); }
+    H5Sget_simple_extent_dims(dataspace_id, dims, nullptr);
+
+    data.clear();
+    std::fill_n(std::back_inserter(data), dims[0], static_cast<T>(0));
+    herr_t status = H5Dread(dataset_id, data_type_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, data.data());
+    if(status < 0) { throw std::runtime_error("ReadArray::H5Dread"); }
+
+    if( H5Dclose(dataset_id) < 0 ) { throw std::runtime_error("ReadArray::H5Dclose"); }
+}
+
+} // namespace wcc
---
+++ tests/H5IOTest.cpp
@@ -0,0 +1,89 @@
+/* H5IOTest.cpp
+* 
+* Author: Wentao Wu
+*/
+
+#include "H5IO.h"
+#include <vector>
+#include <fmt/format.h>
+#include <unistd.h>
+
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+using namespace wcc;
+
+TEST_CASE("H5IOTest", "[WCCommon]") {
+    std::string filename = "H5IOTestData.h5";
+    SECTION("write - array") {
+        std::vector<double> darray{1.1,1.1,2.1,3.1,5.1,8.1,11.1,13.1};
+        std::vector<int   > iarray{1,1,2,3,5,8,11,13};
+        herr_t status;
+        hid_t file_id;
+        file_id = H5Fcreate(filename.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+        if(file_id < 0) throw std::runtime_error("StreamFacade,dump_h5,H5FCreate");
+        hid_t group_id = H5Gcreate (file_id, "Data", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        write_array_to_h5(group_id, "d_dataset", darray.data(), darray.size());
+        write_array_to_h5(group_id, "i_dataset", iarray.data(), iarray.size());
+        H5Gclose(group_id);
+        H5Fclose(file_id);
+        unlink(filename.c_str());
+    }
+    SECTION("scan objects") {
+        std::vector<double> darray{1.1,1.1,2.1,3.1,5.1,8.1,11.1,13.1};
+        std::vector<int   > iarray{1,1,2,3,5,8,11,13};
+        herr_t status;
+        hid_t file_id;
+        file_id = H5Fcreate(filename.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+        if(file_id < 0) throw std::runtime_error("StreamFacade,dump_h5,H5FCreate");
+        hid_t group_id = H5Gcreate (file_id, "Data", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+        write_array_to_h5(group_id, "d_dataset", darray.data(), darray.size());
+        write_array_to_h5(group_id, "i_dataset", iarray.data(), iarray.size());
+        auto objs_from_group = scan_group_object(group_id);
+        REQUIRE(objs_from_group.size() == 2);
+        REQUIRE(objs_from_group[0] == "d_dataset");
+        REQUIRE(objs_from_group[1] == "i_dataset");
+        auto objs_from_root = scan_path_object(file_id, "/");
+        REQUIRE(objs_from_root.size() == 1);
+        REQUIRE(objs_from_root[0] == "Data");
+        auto objs_from_path = scan_path_object(file_id, "/Data");
+        REQUIRE(objs_from_path.size() == 2);
+        REQUIRE(objs_from_path[0] == "d_dataset");
+        REQUIRE(objs_from_path[1] == "i_dataset");
+        H5Gclose(group_id);
+        H5Fclose(file_id);
+        unlink(filename.c_str());
+    }
+    SECTION("write - vector") {
+        std::vector<double> darray{1.1,1.1,2.1,3.1,5.1,8.1,11.1,13.1};
+        std::vector<int   > iarray{1,1,2,3,5,8,11,13};
+        herr_t status;
+        hid_t file_id;
+        file_id = H5Fcreate(filename.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+        if(file_id < 0) throw std::runtime_error("StreamFacade,dump_h5,H5FCreate");
+        write_vector_to_h5(file_id, "/d_dataset", darray, 'z');
+        write_vector_to_h5(file_id, "/i_dataset", iarray, 'z');
+        auto objs = scan_path_object(file_id, "/");
+        REQUIRE(objs.size() == 2);
+        REQUIRE(objs[0] == "d_dataset");
+        REQUIRE(objs[1] == "i_dataset");
+        H5Fclose(file_id);
+        unlink(filename.c_str());
+    }
+    SECTION("write - list") {
+        std::vector<double> dlist{1.1,1.1,2.1,3.1,5.1,8.1,11.1,13.1};
+        std::vector<int   > ilist{1,1,2,3,5,8,11,13};
+        herr_t status;
+        hid_t file_id;
+        file_id = H5Fcreate(filename.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+        if(file_id < 0) throw std::runtime_error("StreamFacade,dump_h5,H5FCreate");
+        write_vector_to_h5(file_id, "/d_dataset", dlist, 'z');
+        write_vector_to_h5(file_id, "/i_dataset", ilist, 'z');
+        auto objs = scan_path_object(file_id, "/");
+        REQUIRE(objs.size() == 2);
+        REQUIRE(objs[0] == "d_dataset");
+        REQUIRE(objs[1] == "i_dataset");
+        H5Fclose(file_id);
+        unlink(filename.c_str());
+    }
+}
