7e5e650912264d6fe817dd8573e25cc56162052a
Peter Liu <liuyg@hejifund.com>
2023-11-12 20:43:36 +0800

    DefTuple: allow 3rd parameter for repetition number of a field

--- include/DefTuple.h
+++ include/DefTuple.h
@@ -1,42 +1,58 @@
 #pragma once
 
-#include <boost/preprocessor/punctuation/comma_if.hpp>
-#include <boost/preprocessor/stringize.hpp>
 #include <boost/preprocessor/seq/for_each.hpp>
-#include <boost/preprocessor/seq/for_each_i.hpp>
 #include <boost/preprocessor/seq/seq.hpp>
-#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/seq/transform.hpp>
+#include <boost/preprocessor/seq/enum.hpp>
 #include <boost/preprocessor/seq/variadic_seq_to_seq.hpp>
+#include <boost/preprocessor/tuple/elem.hpp>
+#include <boost/preprocessor/tuple/size.hpp>
+#include <boost/preprocessor/tuple/pop_back.hpp>
+#include <boost/preprocessor/tuple/replace.hpp>
+#include <boost/preprocessor/repetition/repeat_from_to.hpp>
+#include <boost/preprocessor/control/iif.hpp>
+#include <boost/preprocessor/comparison/equal.hpp>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/stringize.hpp>
 
-#define ELEMENT_TYPE(r, data, n, elem) \
-    BOOST_PP_COMMA_IF(n)               \
-    BOOST_PP_TUPLE_ELEM(0, elem)
-#define ELEMENT_NAME(r, data, elem) \
-    BOOST_PP_TUPLE_ELEM(1, elem),
-#define ELEMENT_NAME_STR(r, data, elem) \
-    BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, elem)),
+#define ELEM_i(s, data, elem) BOOST_PP_TUPLE_ELEM(data, elem)
 
-#define DEF_TUPLE(name, values, names_func)                                                        \
-    using name = std::tuple<                                                                       \
-        BOOST_PP_SEQ_FOR_EACH_I_R(1, ELEMENT_TYPE, , BOOST_PP_VARIADIC_SEQ_TO_SEQ(values))>;       \
-                                                                                                   \
-    struct name##Field                                                                             \
-    {                                                                                              \
-        enum                                                                                       \
-        {                                                                                          \
-            BOOST_PP_SEQ_FOR_EACH_R(1, ELEMENT_NAME, , BOOST_PP_VARIADIC_SEQ_TO_SEQ(values))       \
-        };                                                                                         \
-    };                                                                                             \
-    inline const std::vector<std::string> &names_func()                                            \
-    {                                                                                              \
-        static std::vector<std::string> s_field_names = {                                          \
-            BOOST_PP_SEQ_FOR_EACH_R(1, ELEMENT_NAME_STR, , BOOST_PP_VARIADIC_SEQ_TO_SEQ(values))}; \
-        return s_field_names;                                                                      \
+#define ELEM_1_STR(s, data, elem) BOOST_PP_STRINGIZE(BOOST_PP_TUPLE_ELEM(1, elem))
+
+#define ELEM_POP_BACK(z, n, elem) \
+    BOOST_PP_TUPLE_REPLACE(BOOST_PP_TUPLE_POP_BACK(elem), 1, BOOST_PP_CAT(BOOST_PP_TUPLE_ELEM(1, elem),n))
+
+#define ELEM_FOR(r, data, elem) \
+    BOOST_PP_IIF(BOOST_PP_EQUAL(BOOST_PP_TUPLE_SIZE(elem), 2), elem, \
+        BOOST_PP_REPEAT_FROM_TO(1, BOOST_PP_TUPLE_ELEM(2, elem), ELEM_POP_BACK, elem))
+
+#define EXPAND_SEQ(seq)  BOOST_PP_SEQ_FOR_EACH_R(1, ELEM_FOR, 0, seq)
+
+#define DEF_TUPLE(name, values, names_func)                                                  \
+    using name = std::tuple<BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(ELEM_i, 0,              \
+        BOOST_PP_VARIADIC_SEQ_TO_SEQ(EXPAND_SEQ(BOOST_PP_VARIADIC_SEQ_TO_SEQ(values)))       \
+    ))>;                                                                                     \
+                                                                                             \
+    struct name##Field {                                                                     \
+        enum {                                                                               \
+            BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(ELEM_i, 1,                              \
+              BOOST_PP_VARIADIC_SEQ_TO_SEQ(EXPAND_SEQ(BOOST_PP_VARIADIC_SEQ_TO_SEQ(values))) \
+            ))                                                                               \
+        };                                                                                   \
+    };                                                                                       \
+                                                                                             \
+    inline const std::vector<std::string>& names_func() {                                    \
+        static std::vector<std::string> s_field_names = {                                    \
+            BOOST_PP_SEQ_ENUM(BOOST_PP_SEQ_TRANSFORM(ELEM_1_STR, 1,                          \
+              BOOST_PP_VARIADIC_SEQ_TO_SEQ(EXPAND_SEQ(BOOST_PP_VARIADIC_SEQ_TO_SEQ(values))) \
+            ))                                                                               \
+        };                                                                                   \
+        return s_field_names;                                                                \
     }
 
 /*
  * Usage example:
-
+ 
 namespace wcq {
 
 DEF_TUPLE(
@@ -44,8 +60,8 @@ DEF_TUPLE(
     (seq_id_t     , SeqNo)         // unique in one channel
     (channel_id_t , ChannelNo)
     (timestamp_t  , TransactionTime)
-    (decimal_t    , Price)
-    (volume_t     , Volume)
+    (decimal_t    , Price, 6)
+    (volume_t     , Volume, 6)
     (price_t      , Amount)        // SH only
     (flag_t       , BSFlag)        // SH only 'B' for active buy and 'S' for active sell, 'U' for unknown
     (flag_t       , CancelFlag)    // SZ only 'C' for cancel, 'F' for trade
