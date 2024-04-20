88faa1ffa43471eaf2c5ad23ed11b493029f0d09
Peter Liu <liuyg@hejifund.com>
2024-04-20 13:56:44 +0800

    LogConfig: comments and structure perfection

--- include/LogConfig.h
+++ include/LogConfig.h
@@ -29,24 +29,38 @@
 
 namespace wcc {
 
-struct impl {  // internal stuff; make impl a struct so that static members can be inlined and linked as one unit
+namespace internal {
+
+struct impl {  // make impl a struct so that static members can be inlined and linked as one unit
     struct string_less : std::less<std::string> {
         using is_transparent = void;
         bool operator()(std::string_view sv1, std::string_view sv2) const { return sv1 < sv2; }
     };
 
     inline static std::string s_logger_config_file;
-    inline static boost::container::flat_map<std::string, spdlog::logger, string_less> s_logger_table;
-    inline static std::ostringstream oss;
+    inline static boost::container::flat_map<std::string, spdlog::logger, string_less> s_loggers;
+    inline static std::ostringstream s_oss;
 };  // struct impl
 
+// T must be a pointer (since it is used as has_id<this> for now)
+template <typename T, typename = void> struct has_id : std::false_type {};
+template <typename T> struct has_id<T, std::void_t<decltype((*std::declval<T>()).id())>> : std::true_type {};
+template <typename T>
+auto id(const T* t) {
+    if constexpr (has_id<const T*>::value) return t->id();
+    else return 0; // doesn't matter what returns (both type and value) here since it will be discarded by compiler
+}
+
+} // namespace internal
+
+
 inline spdlog::logger* get_logger(std::string_view logger_name) {
-    auto iter = impl::s_logger_table.find(logger_name);
-    if (iter != impl::s_logger_table.end()) [[likely]]
+    auto iter = internal::impl::s_loggers.find(logger_name);
+    if (iter != internal::impl::s_loggers.end()) [[likely]]
         return &(iter->second);
 
     fmt::print(stderr, "get_logger,MissingLogger,logger={},LogConfig={}\n",
-               logger_name, impl::s_logger_config_file);
+               logger_name, internal::impl::s_logger_config_file);
     throw std::runtime_error(fmt::format("MissingLogger:{}", logger_name));
 }
 
@@ -64,8 +78,8 @@ inline std::chrono::system_clock::duration dur_from_chars(std::string_view dur_s
 
 // Note that every get_logger_str call will clear the underlying string
 inline auto get_logger_str() {
-    auto str_view = impl::oss.view();
-    impl::oss.seekp(0);
+    auto str_view = internal::impl::s_oss.view();
+    internal::impl::s_oss.seekp(0);
     return str_view;
 }
 
@@ -73,7 +87,7 @@ inline void config_log(YAML::Node const& cfg) {
     static bool once = false;
 
     if (once) {
-        fmt::print("loggers has been created before from {}\n", impl::s_logger_config_file);
+        fmt::print("loggers has been created before from {}\n", internal::impl::s_logger_config_file);
         return;
     }
 
@@ -100,14 +114,14 @@ inline void config_log(YAML::Node const& cfg) {
             sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(log_file));
         }
         if (sink == "string") {
-            sinks.push_back(std::make_shared<spdlog::sinks::ostream_sink_mt>(impl::oss));
+            sinks.push_back(std::make_shared<spdlog::sinks::ostream_sink_mt>(internal::impl::s_oss));
         }
     }
 
-    impl::s_logger_table.reserve(cfg["loggers"].size());
+    internal::impl::s_loggers.reserve(cfg["loggers"].size());
     for (auto const &name : cfg["loggers"]) {
         std::string name_str = name.as<std::string>();
-        impl::s_logger_table.emplace(name_str, spdlog::logger{name_str, std::begin(sinks), std::end(sinks)});
+        internal::impl::s_loggers.emplace(name_str, spdlog::logger{name_str, std::begin(sinks), std::end(sinks)});
     }
 
     auto format_str = cfg["default_format"].as<std::string>();
@@ -115,7 +129,7 @@ inline void config_log(YAML::Node const& cfg) {
     auto lvl = spdlog::level::from_str(lvl_str);
     if (lvl == spdlog::level::off)
  	throw std::runtime_error("unknown log level");
-    for (auto [_, logger] : impl::s_logger_table) {
+    for (auto [_, logger] : internal::impl::s_loggers) {
         logger.set_pattern(format_str);
         logger.set_level(lvl);
     }
@@ -126,7 +140,7 @@ inline void config_log(YAML::Node const& cfg) {
     if (!cfg_err_loggers.IsNull()) {
         auto set_error_loggers = cfg_err_loggers.as<std::vector<std::string>>();
         for (const auto &logger_name : set_error_loggers) {
-            auto& logger = impl::s_logger_table.at(logger_name);
+            auto& logger = internal::impl::s_loggers.at(logger_name);
             logger.set_level(spdlog::level::err);
         }
     }
@@ -136,12 +150,12 @@ inline void config_log(YAML::Node const& cfg) {
     if (!cfg_dbg_loggers.IsNull()) {
         auto set_debug_loggers = cfg_dbg_loggers.as<std::vector<std::string>>();
         for (const auto &logger_name : set_debug_loggers) {
-            auto& logger = impl::s_logger_table.at(logger_name);
+            auto& logger = internal::impl::s_loggers.at(logger_name);
             logger.set_level(spdlog::level::debug);
         }
     }
 
-    for (auto [_, logger] : impl::s_logger_table) {
+    for (auto [_, logger] : internal::impl::s_loggers) {
        logger.flush_on(spdlog::level::warn);
     }
 
@@ -150,7 +164,7 @@ inline void config_log(YAML::Node const& cfg) {
 
 #if defined(TEST)
 inline void config_log() {
-    impl::s_logger_config_file = "TEST";
+    internal::impl::s_logger_config_file = "TEST";
     YAML::Node cfg = YAML::Load(R"(
         default_format    : "[%m-%d %H:%M:%S.%f] [%-8l] [%-12n] %v"
         default_level     : "info"
@@ -163,8 +177,8 @@ inline void config_log() {
     )");
 #else
 inline void config_log(std::string_view config_file) {
-    impl::s_logger_config_file = config_file;
-    YAML::Node cfg = YAML::LoadFile(impl::s_logger_config_file);
+    internal::impl::s_logger_config_file = config_file;
+    YAML::Node cfg = YAML::LoadFile(internal::impl::s_logger_config_file);
 #endif
     config_log(cfg);
 }
@@ -260,7 +274,7 @@ inline void log_critical(format_string_t<Args...> fmt, Args &&...args) {
 inline void log_flush() { get_logger("main")->flush(); }
 
 inline void log_flush_all() {
-    for (auto [_, logger] : impl::s_logger_table) {
+    for (auto [_, logger] : internal::impl::s_loggers) {
        logger.flush();
     }
 }
@@ -285,18 +299,6 @@ inline constexpr auto is_inside_method(std::source_location loc) {
     // Not a pattern of static R class::foo() or R foo()
     return name.find("static ") == name.npos && name.find("::") != name.npos;
 }
-
-namespace internal {  // Used by macros following
-    // T must be a pointer (since it is used as has_id<this> for now)
-    template <typename T, typename = void> struct has_id : std::false_type {};
-    template <typename T> struct has_id<T, std::void_t<decltype((*std::declval<T>()).id())>> : std::true_type {};
-    template <typename T>
-    auto id(const T* t) {
-        if constexpr (has_id<const T*>::value) return t->id();
-        else return 0; // doesn't matter what returns (both type and value) here since it will be discarded by compiler
-    }
-
-} // namespace internal
 
 } // namespace wcc
 
@@ -324,6 +326,10 @@ namespace internal {  // Used by macros following
 #define HJ_HAS_ID(is_method) BOOST_PP_IF(is_method, wcc::internal::has_id<decltype(this)>::value, false)
 #define HJ_ID(is_method)     BOOST_PP_IF(is_method, wcc::internal::id(this), '-') // the '-' actually no use, just make grammar happy in HJ_TLOG
 
+/**
+ * HJ_LOG formatted ouput with given level, event, and format-value pairs, using logger
+ * of the current class attached (is_method == 1), or the "main" logger (is_method == 0).
+ */
 #define HJ_LOG(is_method, level, event, ...) {                                                            \
     constexpr auto fun_name = ::wcc::get_source_function_name(std::source_location::current());           \
     HJ_THIS(is_method)log_##level("[{:16.16s}] [{:12.12s}] {{"                                            \
@@ -332,6 +338,12 @@ namespace internal {  // Used by macros following
         BOOST_PP_IF(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), COLLECT_VAR, BOOST_PP_EMPTY)(__VA_ARGS__));      \
 }
 
+/**
+ * HJ_TLOG formatted ouput with given level, event, and format-value pairs, using logger
+ * of the current class attached (is_method == 1), or the "main" logger (is_method == 0),
+ * plus trader id infomation as [tid] if current class has an id() method, or [-] if not.
+ * Note that is_method == 0 means no id() avaiable (since there's event no class/object).
+ */
 #define HJ_TLOG(is_method, level, event, ...) {                                                           \
     constexpr auto fun_name = ::wcc::get_source_function_name(std::source_location::current());           \
     if constexpr (HJ_HAS_ID(is_method)) {                                                                 \
@@ -345,6 +357,21 @@ namespace internal {  // Used by macros following
         "}}", fun_name, event                                                                             \
         BOOST_PP_IF(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), COLLECT_VAR, BOOST_PP_EMPTY)(__VA_ARGS__));      \
     }                                                                                                     \
+}
+
+/**
+ * HJ_LOG_ID formatted ouput with given level, event, and format-value pairs, using logger
+ * of the current class attached (is_method == 1), or the "main" logger (is_method == 0),
+ * plus id infomation (output as [id]) given as 4th macro parameter.
+ *
+ * Used in cases that id can not be obtained as this->id() call but id does exist and need to be output.
+ */
+#define HJ_LOG_ID(is_method, level, event, id, ...) {                                                     \
+    constexpr auto fun_name = ::wcc::get_source_function_name(std::source_location::current());           \
+    HJ_THIS(is_method)log_##level("[{:16.16s}] [{:12.12s}] [{}] {{"                                       \
+        BOOST_PP_IF(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), COLLECT_FORMAT_STR, BOOST_PP_EMPTY)(__VA_ARGS__) \
+        "}}", fun_name, event, id                                                                         \
+        BOOST_PP_IF(BOOST_PP_VARIADIC_SIZE(__VA_ARGS__), COLLECT_VAR, BOOST_PP_EMPTY)(__VA_ARGS__));      \
 }
 
 #define VAR_1(a)      (BOOST_PP_STRINGIZE(a)":{}", a)
