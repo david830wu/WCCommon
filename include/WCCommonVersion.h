aeba8fa1e54449af5221effd2f358a448fef19b2
Wentao Wu <david830wu@gmail.com>
2024-04-30 10:27:03 +0800

    update Version to 1.7.1

--- include/WCCommonVersion.h
+++ include/WCCommonVersion.h
@@ -5,5 +5,5 @@
 
 #pragma once
 
-#define WCCOMMON_VERSION         "1.7.0"
-#define WCCOMMON_VERSION_MESSAGE "Upgrade logger and for C++20"
+#define WCCOMMON_VERSION         "1.7.1"
+#define WCCOMMON_VERSION_MESSAGE "extract HJLogFormat from LogConfig"
